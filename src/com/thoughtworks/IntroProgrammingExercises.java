49ab8f6dee393fea082c39ac53e6f155512c834e
Qwill Duvall <saltybear14@yahoo.com>
2014-09-11 18:32:19 -0500

    All Diamonds and FizzBuzz works

--- src/com/thoughtworks/IntroProgrammingExercises.java
+++ src/com/thoughtworks/IntroProgrammingExercises.java
@@ -82,6 +82,117 @@ public class IntroProgrammingExercises {
         }
     }
 
+    //Diamond
+    //Given a number n, print a centered diamond. Example for n=3:
+    public void diamond(int n){
+        int height = n + n - 1;
+        int width = 1;
+        int whiteSpace = n - 1;
+        boolean goingUp = true;
+        while (height > 0) {
+
+            int w = width;
+            int s = whiteSpace;
+            while(s > 0){
+                System.out.print(" ");
+                s--;
+            }
+            while (w >  0) {
+                System.out.print("*");
+                w--;
+            }
+            System.out.println();
+            if (n == 1){
+                goingUp = false;
+            }
+            if (goingUp == true){
+                width+=2;
+                whiteSpace--;
+                height --;
+                n--;
+
+            }
+            else {
+                width -= 2;
+                whiteSpace++;
+                height--;
+                n++;
+            }
+        }
+
+    }
+
+    //Diamond with Name
+    //Given a number n, print a centered diamond with your name in place of the middle line. Example for n=3:
+    public void diamondWithName(int n){
+        int height = n + n - 1;
+        int width = 1;
+        int whiteSpace = n - 1;
+        boolean goingUp = true;
+        while (height > 0) {
+
+            if (n == 1) {
+                System.out.print("Qwill");
+                goingUp = false;
+            } else {
+                int w = width;
+                int s = whiteSpace;
+                while (s > 0) {
+                    System.out.print(" ");
+                    s--;
+                }
+                while (w > 0) {
+                    System.out.print("*");
+                    w--;
+                }
+            }
+            if (goingUp == true) {
+                width += 2;
+                whiteSpace--;
+                height--;
+                n--;
+
+            } else {
+                width -= 2;
+                whiteSpace++;
+                height--;
+                n++;
+            }
+            System.out.println();
+        }
+    }
+
+    /*
+     *FizzBuzz Exercise
+     *
+     *FizzBuzz is a simple number game where you count, but say "Fizz" and/or "Buzz" instead of numbers adhering to certain rules.
+     *
+     *Create a FizzBuzz() method that prints out the numbers 1 through 100.
+     *Instead of numbers divisible by three print "Fizz".
+     *Instead of numbers divisible by five print "Buzz".
+     *Instead of numbers divisible by three and five print "FizzBuzz".
+     */
+    public void fizzBuzz(){
+        int i = 0;
+        while (i<=100) {
+            if (i%3 == 0 && i%5 == 0){
+                System.out.println("FizzBuzz");
+            }
+            else if (i%3 == 0){
+                System.out.println("Fizz");
+            }
+            else if (i%5 == 0){
+                System.out.println("Buzz");
+            }
+            else {
+                System.out.println(i);
+            }
+            i++;
+        }
+
+    }
+
+
     public static void main(String[] args) {
         IntroProgrammingExercises exercises = new IntroProgrammingExercises();
         exercises.easiestExerciseEver();
@@ -91,5 +202,10 @@ public class IntroProgrammingExercises {
 
         System.out.println(" ");
         exercises.isoscelesTriangle(3);
+        exercises.diamond(6);
+        exercises.diamondWithName(3);
+
+        exercises.fizzBuzz();
+
     }
 }
